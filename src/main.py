b61172404ab30eca042bf3777d225857ecd5b249
Zac Orndorff <zorndorff@gmail.com>
2022-03-05 15:32:53 -0600

    do some random stuff

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 import pyautogui;
+import random;
 
+RANDOM_RANGEX=100
+RANDOM_RANGEY=100
 
-pyautogui.alert("This worked!")
+pyautogui.keyDown('w') 
+
+while True:
+    randX=random.randrange(-RANDOM_RANGEX, RANDOM_RANGEX)
+    randY=random.randrange(-RANDOM_RANGEY, RANDOM_RANGEY)
+    print(f"Randomly moving X:{randX} Y:{randY}")
+    pyautogui.moveRel(randX, randY, duration=3)  # move mouse relative to its current position
+    pyautogui.leftClick(x=None, y=None, interval=1, duration=3)
